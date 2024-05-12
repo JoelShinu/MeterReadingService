dcd2edc46c22c5bd8e2930da4cf027b8c176a6e6
JoelShinu <joelshinu@yahoo.com>
2024-05-12 15:53:33 +0100

    delete DTO

--- api/DTO/MeterReadingDto.cs
+++
@@ -1,9 +0,0 @@
-namespace api.DTO
-{
-    public class MeterReadingDto
-    {
-        public int AccountId { get; set; }
-        public DateTime MeterReadingDateTime { get; set; }
-        public int MeterReadingValue { get; set; }
-    }
-}
--- api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ae621685feebe1d23164e0213b0b6a43b6ff9e1b")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
